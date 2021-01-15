e24fcfd4548900528c93af552dfa23df1e1eb30a
Saffat Aziz <saziz028@uottawa.ca>
2021-01-16 03:38:30 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -56,35 +56,29 @@ function getMovie(){
     let movieId = sessionStorage.getItem('movieId');
     axios.get(`http://www.omdbapi.com/?i=${movieId}&apikey=fb26a757`)
     .then((response) => {
-        let movies = response.data.Search
-        let output = ''
-        $.each(movies, (index, movie)=>{
-            output += `
+        let movie = response.data;
+        let result = `
+        <div class = "row">
             <div class = "col-md-4">
-                <div class = "well text-center">
-                    <img src = "${movie.Poster}">
-                    <br></br>
-                    <h5>${movie.Title}</h5>
-                    <div class = buttons>
-                        <a onClick = "movieSelected('${movie.imdbID}')" class = "btn btn-primary btn-sm" id = "button" href = "#">Movie Details</a>
-                        <a  class = "btn btn-primary btn-sm" id = "button" href = "#">Nominate movie</a>
-                        <br></br>
-                    </div>
-                    
-                </div>
+                <img src = "${movie.Poster}" class = "thumbnail">
             </div>
-            `
-        })
-        if (typeof movies == 'undefined'){
-            let result = `<h6 class = "result"> No Movies Found. Try another movie </h6>`
-            $('#movies').html(result)
-            return
-        }
-        $('#movies').html(output)
-        console.log(movies)
+            <div class = "col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul>class = "list-group">
+                <li class = "list-group-item"><strong>Genre</strong>${movie.Genre}</li>
+                <li class = "list-group-item"><strong>Year</strong>${movie.Year}</li>
+                <li class = "list-group-item"><strong>Released</strong>${movie.Released}</li>
+                <li class = "list-group-item"><strong>Rated</strong>${movie.Rated}</li>
+                <li class = "list-group-item"><strong>IMDB Rating</strong>${movie.imdbRating}</li>
+                <li class = "list-group-item"><strong>Director</strong>${movie.Director}</li>
+                <li class = "list-group-item"><strong>Writer</strong>${movie.Writer}</li>
+                <li class = "list-group-item"><strong>Actors</strong>${movie.Actors}</li>
+                <li class = "list-group-item"><strong>Runtime</strong>${movie.Runtime}</li>
+                <li class = "list-group-item"><strong>Language</strong>${movie.Language}</li>
 
-
-
+            </ul>
+            
+        `
     })
     .catch((error) => {
         console.error(error)
