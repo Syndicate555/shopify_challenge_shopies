f1368ddcfcfea3ef0d45c3efc964468ed08e1626
Saffat Aziz <saziz028@uottawa.ca>
2021-01-16 03:59:43 +0600

    changes

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ function fetchMovies(searchMovie){
 
 function movieSelected(id){
     sessionStorage.setItem('movieId', id); // Saving the movie ID to the session
-    window.location = movie.html;
+    window.location = 'movies.html';
     return false;
 }
 
@@ -63,22 +63,32 @@ function getMovie(){
                 <img src = "${movie.Poster}" class = "thumbnail">
             </div>
             <div class = "col-md-8">
-            <h2>${movie.Title}</h2>
-            <ul>class = "list-group">
-                <li class = "list-group-item"><strong>Genre</strong>${movie.Genre}</li>
-                <li class = "list-group-item"><strong>Year</strong>${movie.Year}</li>
-                <li class = "list-group-item"><strong>Released</strong>${movie.Released}</li>
-                <li class = "list-group-item"><strong>Rated</strong>${movie.Rated}</li>
-                <li class = "list-group-item"><strong>IMDB Rating</strong>${movie.imdbRating}</li>
-                <li class = "list-group-item"><strong>Director</strong>${movie.Director}</li>
-                <li class = "list-group-item"><strong>Writer</strong>${movie.Writer}</li>
-                <li class = "list-group-item"><strong>Actors</strong>${movie.Actors}</li>
-                <li class = "list-group-item"><strong>Runtime</strong>${movie.Runtime}</li>
-                <li class = "list-group-item"><strong>Language</strong>${movie.Language}</li>
-
-            </ul>
-            
+                <h2>${movie.Title}</h2>
+                <ul class = "list-group">
+                    <li class = "list-group-item"><strong>Genre</strong>${movie.Genre}</li>
+                    <li class = "list-group-item"><strong>Year</strong>${movie.Year}</li>
+                    <li class = "list-group-item"><strong>Released</strong>${movie.Released}</li>
+                    <li class = "list-group-item"><strong>Rated</strong>${movie.Rated}</li>
+                    <li class = "list-group-item"><strong>IMDB Rating</strong>${movie.imdbRating}</li>
+                    <li class = "list-group-item"><strong>Director</strong>${movie.Director}</li>
+                    <li class = "list-group-item"><strong>Writer</strong>${movie.Writer}</li>
+                    <li class = "list-group-item"><strong>Actors</strong>${movie.Actors}</li>
+                    <li class = "list-group-item"><strong>Runtime</strong>${movie.Runtime}</li>
+                    <li class = "list-group-item"><strong>Language</strong>${movie.Language}</li>
+                </ul>
+            </div>
+        </div>
+        <div class = "row">
+            <div class = "well">
+                <h3>Plot</h3>
+                ${movie.Plot}
+                <hr>
+                <a href = "http://imdb.com/title/${movie.imdbID}" target = "_blank" class = "btn btn-primary">View IMDB Profile</a>
+                <a href = "index.html" class = "btn btn-default">Go Back to Search Page</a>
+            </div>
+        </div>
         `
+        $('#movie').html(result);
     })
     .catch((error) => {
         console.error(error)
