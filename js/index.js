86e2d4d04837bbbfc3d7b277c6b9c104a0133834
Saffat Aziz <saziz028@uottawa.ca>
2021-01-15 23:58:03 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 $(document).ready(()=>{
     $('#searchForm').on('submit', (s) =>{
         let searchMovie = ($('#searchText').val());
+        fetchMovies(searchMovie);
         s.preventDefault()
     })
 })
+
+function fetchMovies(){
+    
+}
